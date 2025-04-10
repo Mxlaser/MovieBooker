ae9f1b1158cb05670eb455c2a635f3f55637ac6d
Nikola.Z <nzivkovic@eig.fr>
2025-04-10 18:09:36 +0200

    1

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'https://moviebooker-api.onrender.com/api',
+  baseURL: 'https://moviebooker-api.onrender.com',
 });
 
 // Ajouter le token si disponible
